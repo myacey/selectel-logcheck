289ca4cafbeb0da70547ea72e4e971f2e779c639
myacey <grg1nev@gmail.com>
2026-02-14 18:52:21 +0300

    change golangci-lint integration

---
+++ pkg/logcheck/golinters/logcheck.go
@@ -0,0 +1,12 @@
+package golinters
+
+import (
+	"github.com/myacey/selectel-logcheck/pkg/logcheck"
+	"golang.org/x/tools/go/analysis"
+)
+
+func New(conf any) ([]*analysis.Analyzer, error) {
+	return []*analysis.Analyzer{
+		logcheck.Analyzer,
+	}, nil
+}
